5a85433dbc54bea6a3ce8d303114e29c130c3347
tboonma <tboonma@hotmail.com>
2023-05-10 12:17:21 +0700

    Done Column-Level Encryption

---
+++ README.md
@@ -0,0 +1,7 @@
+# Book
+
+#### Run project locally
+You need to define the following environment variables
+```
+DATABASE_SECRET=YOUR_KEY
+```
--- src/main/java/ku/book/model/User.java
+++ src/main/java/ku/book/model/User.java
@@ -1,11 +1,11 @@
 package ku.book.model;
 
+import ku.book.config.AttributeEncryptor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnTransformer;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.Instant;
 import java.util.UUID;
 
@@ -18,8 +18,19 @@ public class User {
     @GeneratedValue
     private UUID id;
 
+    @Column(columnDefinition="VARBINARY(256)")
+    @ColumnTransformer(
+            read = "cast(AES_DECRYPT(username, 'key') as char(255))",
+            write = "AES_ENCRYPT(?, 'key')"
+    )
     private String username;
     private String password;
+
+    @Column(columnDefinition="VARBINARY(256)")
+    @ColumnTransformer(
+            read = "cast(AES_DECRYPT(name, 'key') as char(255))",
+            write = "AES_ENCRYPT(?, 'key')"
+    )
     private String name;
 
     private Instant createdAt;
